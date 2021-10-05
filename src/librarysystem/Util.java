405b072f3bf2bf5a92a468148873cdb2ec878763
Win-Ei-Khaing <wineikhaing111111@gmail.com>
2021-10-05 18:24:57 -0500

    login

--- src/librarysystem/Util.java
+++ src/librarysystem/Util.java
@@ -19,11 +19,19 @@ public class Util {
 	public static final Color LINK_AVAILABLE = DARK_BLUE;
 	public static final Color LINK_NOT_AVAILABLE = Color.gray;
 	//rgb(18, 75, 14)
-	
+
 	public static Font makeSmallFont(Font f) {
-        return new Font(f.getName(), f.getStyle(), (f.getSize()-2));
-    }
-	
+		return new Font(f.getName(), f.getStyle(), (f.getSize()-2));
+	}
+
+	public static void makeBigFont(JLabel label, Color color) {
+		Font f = new Font(label.getFont().getName(), 
+				label.getFont().getStyle(), (label.getFont().getSize()+5));
+		label.setFont(f);
+		label.setForeground(color);
+
+	}
+
 	public static void adjustLabelFont(JLabel label, Color color, boolean bigger) {
 		if(bigger) {
 			Font f = new Font(label.getFont().getName(), 
@@ -35,14 +43,14 @@ public class Util {
 			label.setFont(f);
 		}
 		label.setForeground(color);
-		
+
 	}
 	/** Sorts a list of numeric strings in natural number order */
 	public static List<String> numericSort(List<String> list) {
 		Collections.sort(list, new NumericSortComparator());
 		return list;
 	}
-	
+
 	static class NumericSortComparator implements Comparator<String>{
 		@Override
 		public int compare(String s, String t) {
@@ -55,7 +63,7 @@ public class Util {
 			else return 1;
 		}
 	}
-	
+
 	public static boolean isNumeric(String s) {
 		if(s==null) return false;
 		try {
